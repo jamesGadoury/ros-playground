a51e303b31bb480c3a71f3b72637516953a6e561
James Gadoury <gadouryjames@gmail.com>
2023-10-21 12:40:54 -0400

    Use tf2 to rotate automatically

--- src/vis_rotations/src/vis_rotations.cpp
+++ src/vis_rotations/src/vis_rotations.cpp
@@ -3,42 +3,37 @@
 #include <memory>
 #include <string>
 
+#include <Eigen/Dense>
 #include <rclcpp/rclcpp.hpp>
-#include "geometry_msgs/msg/pose_stamped.hpp"
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <tf2/LinearMath/Scalar.h>
+#include <tf2_eigen/tf2_eigen.hpp>
 
 using namespace std;
 using namespace std::chrono_literals;
 
 static const string WORLD_FRAME = "world";
 
-geometry_msgs::msg::PoseStamped generate_default_pose_message()
-{
-    auto message = geometry_msgs::msg::PoseStamped();
-    message.header.frame_id = WORLD_FRAME;
-
-    message.pose.position.x = 0;
-    message.pose.position.y = 0;
-    message.pose.position.z = 0;
-
-    return message;
-}
-
 class RotationsVisPublisher : public rclcpp::Node
 {
 public:
     RotationsVisPublisher() : Node("rotations_vis_publisher")
     {
+        pose.setData(Eigen::Affine3d::Identity());
+        pose.frame_id_ = WORLD_FRAME;
         pose_publisher = create_publisher<geometry_msgs::msg::PoseStamped>("pose", 10);
         update_timer = create_wall_timer(500ms, bind(&RotationsVisPublisher::update, this));
     }
 
 private:
+    tf2::Stamped<Eigen::Affine3d> pose;
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_publisher;
     rclcpp::TimerBase::SharedPtr update_timer;
 
     void update()
     {
-        pose_publisher->publish(geometry_msgs::msg::PoseStamped());
+        pose.rotate(Eigen::AngleAxisd(tf2Radians(90.0), Eigen::Vector3d::UnitZ()));
+        pose_publisher->publish(tf2::toMsg(pose));
     }
 };
 
