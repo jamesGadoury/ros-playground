7f9da35e25b39e7cec749d3ac52328d8efb62eb5
James Gadoury <gadouryjames@gmail.com>
2023-12-01 22:41:30 -0500

    Use ignition

--- src/maze_sim/launch/maze_world.launch.py
+++ src/maze_sim/launch/maze_world.launch.py
@@ -7,62 +7,37 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
-
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    launch_file_dir = os.path.join(
-        get_package_share_directory('turtlebot3_gazebo'), 'launch')
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
-
-    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    x_pose = LaunchConfiguration('x_pose', default='6.0')
-    y_pose = LaunchConfiguration('y_pose', default='-1.0')
-    yaw_pose = LaunchConfiguration('yaw_pose', default='3.141592653589793')
+    pkg_ros_gz_sim = get_package_share_directory("ros_gz_sim")
 
     world = os.path.join(
         get_package_share_directory('maze_sim'),
         'worlds',
-        'maze_world.sdf'
-    )
-
-    gzserver_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')
-        ),
-        launch_arguments={'world': world}.items()
-    )
-
-    gzclient_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')
-        )
-    )
-
-    robot_state_publisher_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(launch_file_dir, 'robot_state_publisher.launch.py')
-        ),
-        launch_arguments={'use_sim_time': use_sim_time}.items()
-    )
-
-    spawn_turtlebot_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(os.path.join(
-                get_package_share_directory('maze_sim'), 'launch'), 'better_spawn_turtlebot3.launch.py')
-        ),
-        launch_arguments={
-            'x_pose': x_pose,
-            'y_pose': y_pose,
-            'yaw_pose': yaw_pose
-        }.items()
+        'maze_world_ign.sdf'
     )
 
     ld = LaunchDescription()
 
     # Add the commands to the launch description
-    ld.add_action(gzserver_cmd)
-    ld.add_action(gzclient_cmd)
-    ld.add_action(robot_state_publisher_cmd)
-    ld.add_action(spawn_turtlebot_cmd)
+    ld.add_action(IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_ros_gz_sim, "launch", "gz_sim.launch.py")
+        ),
+        launch_arguments={"gz_args": world}.items()
+    ))
+
+    ld.add_action(Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/cmd_vel@geometry_msgs/msg/Twist@ignition.msgs.Twist"]
+    ))
+
+    ld.add_action(Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=["/lidar@sensor_msgs/msg/LaserScan@ignition.msgs.LaserScan"]
+    ))
 
     return ld
