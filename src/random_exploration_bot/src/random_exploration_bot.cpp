0fb42027b398be4c45d32ca81c454c06ac54ea57
James Gadoury <gadouryjames@gmail.com>
2023-12-02 20:57:23 -0500

    slow down update rate

--- src/random_exploration_bot/src/random_exploration_bot.cpp
+++ src/random_exploration_bot/src/random_exploration_bot.cpp
@@ -17,14 +17,20 @@ public:
         publisher_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
         subscription_ = create_subscription<sensor_msgs::msg::LaserScan>("lidar", 10, std::bind(&RandomExploration::laser_scan_callback, this, _1));
         //! @todo configurable name param?
-        data_out_ = std::ofstream("laser_scan.csv");
+        data_out_ = std::ofstream("bot_data_out.csv");
         gen_ = std::mt19937(42);
+        last_update_ = std::chrono::steady_clock::now();
     }
 
 private:
     void laser_scan_callback(const sensor_msgs::msg::LaserScan &msg)
     {
         auto start = std::chrono::steady_clock::now();
+        if (std::chrono::duration_cast<std::chrono::seconds>(start - last_update_) < 1s)
+        {
+            return;
+        }
+
         data_out_ << msg.header.stamp.nanosec << "," 
                         << msg.header.frame_id << ","
                         << msg.angle_min << ","
@@ -46,7 +52,7 @@ private:
         }
 
         auto message = geometry_msgs::msg::Twist();
-        message.linear.x = std::uniform_real_distribution<>(-0.22, 0.22)(gen_);
+        message.linear.x = std::uniform_real_distribution<>(0.0, 0.22)(gen_);
         message.angular.z = std::uniform_real_distribution<>(-2.84, 2.84)(gen_);
         publisher_->publish(message);
         data_out_ << message.linear.x << "," << message.angular.z << ",";
@@ -54,10 +60,13 @@ private:
         data_out_ << "\n";
         auto dur_ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now()-start).count();
         RCLCPP_INFO(get_logger(), "I took %li ms to process laser scan...", dur_ms);
+
+        last_update_ = std::chrono::steady_clock::now();
     }
 
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr subscription_;
+    std::chrono::time_point<std::chrono::steady_clock> last_update_;
     std::ofstream data_out_;
 
     std::mt19937 gen_;
